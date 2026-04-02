3dbcac58e262379faaddea9464ccd1e90ed0763f
vipmichalek <paczekmichi13@gmail.com>
2026-04-02 14:30:58 +0200

    Create disk_load.asm

---
+++ disk_load.asm
@@ -0,0 +1,12 @@
+load_kernel:
+    mov bx, KERNEL_OFFSET ; Gdzie zapisać dane
+    mov dh, 0x1F             ; to są sektory
+    mov dl, [BOOT_DRIVE]
+    
+    mov ah, 0x02 ; czytaj
+    mov al, dh
+    mov ch, 0x00 ; cylinder 0
+    mov dh, 0x00 ; głowica 0
+    mov cl, 0x02 ; zaczyna od drugiego
+    int 0x13
+    ret

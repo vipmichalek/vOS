f373d7dc390c8b3a735dd299fff535e4293500eb
vipmichalek <paczekmichi13@gmail.com>
2026-04-03 20:16:48 +0200

    fixed build.sh to work with newest files

--- build.sh
+++ build.sh
@@ -7,10 +7,14 @@ nasm kernel/interrupts.asm -f elf32 -o tmp/interrupts.o
 echo [2/5] c
 gcc -m32 -ffreestanding -fno-jump-tables -fno-pic -fno-pie -no-pie -fno-stack-protector -Ikernel -c kernel/io.c -o tmp/io.o
 gcc -m32 -ffreestanding -fno-jump-tables -fno-pic -fno-pie -no-pie -fno-stack-protector -Ikernel -c kernel/vga.c -o tmp/vga.o
+gcc -m32 -ffreestanding -fno-jump-tables -fno-pic -fno-pie -no-pie -fno-stack-protector -Ikernel -c kernel/timer.c -o tmp/timer.o
+gcc -m32 -ffreestanding -fno-jump-tables -fno-pic -fno-pie -no-pie -fno-stack-protector -Ikernel -c kernel/ata.c -o tmp/ata.o
+gcc -m32 -ffreestanding -fno-jump-tables -fno-pic -fno-pie -no-pie -fno-stack-protector -Ikernel -c kernel/idt.c -o tmp/idt.o
+gcc -m32 -ffreestanding -fno-jump-tables -fno-pic -fno-pie -no-pie -fno-stack-protector -Ikernel -c kernel/functions.c -o tmp/functions.o
 gcc -m32 -ffreestanding -fno-jump-tables -fno-pic -fno-pie -no-pie -fno-stack-protector -Ikernel -c kernel/kernel.c -o tmp/kernel.o -fno-jump-tables
 
 echo [3/5] link
-ld -m elf_i386 -Ttext 0x1000 tmp/kernel_entry.o tmp/interrupts.o tmp/kernel.o tmp/vga.o tmp/io.o -o tmp/kernel.tmp
+ld -m elf_i386 -Ttext 0x1000 tmp/kernel_entry.o tmp/interrupts.o tmp/kernel.o tmp/vga.o tmp/io.o tmp/timer.o tmp/ata.o tmp/idt.o tmp/functions.o -o tmp/kernel.tmp
 
 echo [4/5] binary
 objcopy -O binary tmp/kernel.tmp kernel.bin

922a29873f059e7ee1b5cd6d37d2701bc8684da8
vipmichalek <paczekmichi13@gmail.com>
2026-04-03 11:35:36 +0200

    more functions in vga.c

--- kernel/vga.h
+++ kernel/vga.h
@@ -4,5 +4,8 @@
 //dawne vga funkcje nie używane wyjebałem je z vga.c
 extern unsigned char* back_buffer;
 void screen_update();
-
+void put_pixel(int x, int y, int r, int g, int b);
+void draw_char(int x, int y, unsigned char font_chars[10], int color);
+void clear_screen_gfx();
+void kprint_char_gfx(char c, int x, int y, int color) ;
 #endif
